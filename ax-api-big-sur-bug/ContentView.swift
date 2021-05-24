98830243b66cbe131e09a95acdd09ba610ffe212
Guill Lo <guill.bout@gmail.com>
2021-05-24 20:03:26 +0800

    create Xcode proj

---
+++ ax-api-big-sur-bug/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  ax-api-big-sur-bug
+//
+//  Created by Guillaume Leclerc on 24/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ax-api-big-sur-bugTests/ax_api_big_sur_bugTests.swift
@@ -0,0 +1,33 @@
+//
+//  ax_api_big_sur_bugTests.swift
+//  ax-api-big-sur-bugTests
+//
+//  Created by Guillaume Leclerc on 24/05/2021.
+//
+
+import XCTest
+@testable import ax_api_big_sur_bug
+
+class ax_api_big_sur_bugTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
